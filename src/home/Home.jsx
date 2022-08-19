ef46e78e1ba3d2bdbc1f42dfd84c3fdf1243ba71
Cao Minh Tri <caominhtri112201@gmail.com>
2022-08-19 16:56:27 +0700

    featured movie

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,44 +1,15 @@
 import React from 'react'
+import Featured from '../components/featured/Featured'
 import Navbar from '../components/navbar/Navbar'
 import "./home.scss"
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <Navbar />
-      <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
-    </div>
-  )
+    return (
+        <div className='home'>
+            <Navbar />
+            <Featured />
+        </div>
+    )
 }
 
 export default Home
