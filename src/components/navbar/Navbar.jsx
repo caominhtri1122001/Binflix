3e6e8b7fea02993b1b212ce7a4b9248b0b03394a
Cao Minh Tri <caominhtri112201@gmail.com>
2022-08-19 10:35:48 +0700

    create the navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import "./navbar.scss";
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from "react";
+
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    }
+
+    return (
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+            <div className="container">
+                <div className="left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
+                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <SearchIcon className="icon" />
+                    <span>KID</span>
+                    <NotificationsIcon className="icon" />
+                    <img src="https://avatars.githubusercontent.com/u/53614632?s=400&u=c439839ff1adc191b11165ac0c26effeb36a1f67&v=4" alt="" />
+                    <div className="profile">
+                        <ArrowDropDownIcon className="icon" />
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Logout</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/home/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+      <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, iste at doloremque vitae culpa praesentium mollitia inventore similique assumenda minus expedita obcaecati sed optio quod, sequi possimus consequatur ab alias.
+    </div>
+  )
+}
+
+export default Home
