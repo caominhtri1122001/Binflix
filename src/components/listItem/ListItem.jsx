3cea8a125b168e3497adcb4269ba1daccb81dbfc
Cao Minh Tri <caominhtri112201@gmail.com>
2022-08-23 15:54:16 +0700

    Slider

---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./listItem.scss";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
+
+const ListItem = () => {
+  return (
+    <div className='listItem'>
+      <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrowIcon />
+          <AddIcon />
+          <ThumbUpOutlinedIcon />
+          <ThumbDownOffAltOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListItem
